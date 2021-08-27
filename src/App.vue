e9152489d302197915f3a93f0b014ee3e81b1366
adilhanar7 <adilhanarbudu7@gmail.com>
2021-08-27 21:58:51 +0300

    Add catalogFilms

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
  <div class="container">
-    <div id="nav">
-    <router-link to="/1">1</router-link> |
-    <router-link to="/2">2</router-link> |
-    <router-link to="/3">3</router-link> |
-    <router-link to="/4">4</router-link> |
-    <router-link to="/5">5</router-link> |
-    <router-link to="/6">6</router-link> |
-    <router-link to="/7">7</router-link>
+  <div id="nav">
+    <router-link :to="{ name: 'CatalogFilms' }">5</router-link> | 
   </div>
   <router-view/>
  </div>
@@ -16,19 +10,30 @@
 <style lang="scss">
 @import '@/assets/scss/index.scss';
 #nav{
-    display: flex;
-    justify-content: center;
-    a{
-    text-decoration: none;
-    color: inherit;
-    margin: 0 15px;
-  }
+  display: flex;
+  justify-content: center;
+  a{
+  text-decoration: none;
+  color: inherit;
+  margin: 0 15px;
+}
+
 }
 .container {
   max-width: vw(1510);
   margin: 0 auto;
 }
-#nav.router-link-exact-active {
-  color: #42b983;
+
+#nav .router-link-exact-active {
+      color: #42b983;
+}
+a{
+    text-decoration: none;
+}
+ul{
+    list-style-type: none;
+}
+h1,h3,h2,h5,h6{
+  margin: 0;
 }
 </style>
