894ab1d550b860bd7ab4a5e72ab1305126b5271a
Sinonium <sinoniumx@gmail.com>
2021-09-07 11:04:32 +0600

    page three started

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
- <div class="container">
   <div id="nav">
     <router-link to="/1">1</router-link> |
     <router-link to="/2">2</router-link> |
@@ -9,15 +8,29 @@
     <router-link to="/6">6</router-link> |
     <router-link to="/7">7</router-link> 
   </div>
+<Header />
+ <div class="container">
+   <Modal />
   <router-view/>
+  <Footer/>
  </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+import Modal from './components/Modal.vue'
+import Footer from './components/Footer.vue'
+export default {
+  components: {Header, Modal, Footer}
+}
+</script>
 
 
 <style lang="scss">
 @import '@/assets/scss/index.scss';
 body{
   background: $bg-main;
+  margin: 0;
 }
 ul{
   padding: 0;
@@ -29,7 +42,7 @@ ul{
 #nav{
   display: flex;
   justify-content: center;
-  margin-bottom: 60px;
+  margin: 30px;
   a{
   text-decoration: none;
   color: inherit;
